4a760cc2e9caf62f488e271678214f712624d58a
Jose Chavez <jose.chavez@dialexa.com>
2024-08-09 14:55:09 -0700

    Added title and Add Item button

--- app/src/main/java/com/rangerforce/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/rangerforce/shoppinglistapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rangerforce.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ShoppingListApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShoppingListApp(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Shopping List",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text("Add Item")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingListAppTheme {
-        Greeting("Android")
+        ShoppingListApp()
     }
 }
